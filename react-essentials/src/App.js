ab310be166b4049fb1a3b59cd62ffc39267e6446
Alonso Rodriguez <alonso.rodriguez.ponce@gmail.com>
2022-03-22 15:22:06 -0600

    React Components

---
+++ react-essentials/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import restaurant from "./restaurant.jpg";
+
+function Header(props){
+  return(
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(props){
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+      <img src={restaurant} height={200}/>
+      <ul style={{ textAlign:"left"}}>
+        {props.dishes.map((dish)=> (
+        <li key={dish.key}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props){
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables",
+  "Minestrone"
+];
+
+const dishObjects = dishes.map((dish,i) => ({id: i, title: dish}));
+console.log(dishObjects);
+function App() {
+  return (
+    <div className="App">
+      <Header name="Alonso"/>
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()}/>
+    </div>
+  );
+}
+
+export default App;
